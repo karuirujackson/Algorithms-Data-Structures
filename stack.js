a12e0831cef8115c28d4b0739741144d7a6d1eb9
Karuiru Jackson <jkjackaruiru641@gmail.com>
2025-02-27 11:55:59 +0300

    Queue Data Structure

---
+++ stack.js
@@ -0,0 +1,8 @@
+//Stack data structure is a sequential collection of elements that follows the principle of the Last In First Out(LIFO).
+//The last element added to the stack is the first one to be removed.
+//Supports 2 main operations:
+//--------Push: Adds an element to the top of the stack.--------
+//--------Pop: Removes the top element from the stack and returns it.------
+//--------Peek: Returns the top element of the stack without removing it.-------
+//The stack is implemented using an array or a linked list data structure.
+//Usage: The stack is often used in algorithms like Browser backtracking, undo/redo while typing, expression conversions, and Call stack in JS runtime.
